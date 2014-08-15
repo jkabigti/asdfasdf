4aeaf6dd3668aa472d6c578a1ae14b5a4a76dc8b
jkabigti <jkabigti@ucsd.edu>
2014-08-14 19:15:37 -0700

    Initial Commit

---
+++ SL136/BL/CourseService.cs
@@ -0,0 +1,23 @@
+﻿namespace Service
+{
+    using System.Collections.Generic;
+
+    using IRepository;
+
+    using POCO;
+
+    public class CourseService
+    {
+        private readonly ICourseRepository repository;
+
+        public CourseService(ICourseRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public List<Course> GetCourseList(ref List<string> errors)
+        {
+            return this.repository.GetCourseList(ref errors);
+        }
+    }
+}
---
+++ SL136/IDAL/ICourseRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace IRepository
+{
+    using System.Collections.Generic;
+
+    using POCO;
+
+    public interface ICourseRepository
+    {
+        List<Course> GetCourseList(ref List<string> errors);
+    }
+}
