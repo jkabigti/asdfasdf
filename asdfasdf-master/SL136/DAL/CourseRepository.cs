994b77276a19b1196f0857400e0cde8a741f8c3d
kjl011 <kjl011@acsmail.ucsd.edu>
2014-08-15 13:18:56 -0700

    initial

---
+++ asdfasdf-master/SL136/DAL/CourseRepository.cs
@@ -0,0 +1,67 @@
+﻿namespace Repository
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    using IRepository;
+
+    using POCO;
+
+    public class CourseRepository : BaseRepository, ICourseRepository
+    {
+        private const string GetCourseListProcedure = "spGetCourseList";
+
+        public List<Course> GetCourseList(ref List<string> errors)
+        {
+            var conn = new SqlConnection(ConnectionString);
+            var courseList = new List<Course>();
+
+            try
+            {
+                var adapter = new SqlDataAdapter(GetCourseListProcedure, conn)
+                                  {
+                                      SelectCommand =
+                                          {
+                                              CommandType = CommandType.StoredProcedure
+                                          }
+                                  };
+
+                var dataSet = new DataSet();
+                adapter.Fill(dataSet);
+
+                if (dataSet.Tables[0].Rows.Count == 0)
+                {
+                    return null;
+                }
+
+                for (var i = 0; i < dataSet.Tables[0].Rows.Count; i++)
+                {
+                    var course = new Course
+                                     {
+                                         CourseId = dataSet.Tables[0].Rows[i]["course_id"].ToString(),
+                                         Title = dataSet.Tables[0].Rows[i]["course_title"].ToString(),
+                                         CourseLevel =
+                                             (CourseLevel)
+                                             Enum.Parse(
+                                                 typeof(CourseLevel),
+                                                 dataSet.Tables[0].Rows[i]["course_level"].ToString()),
+                                         Description = dataSet.Tables[0].Rows[i]["course_description"].ToString()
+                                     };
+                    courseList.Add(course);
+                }
+            }
+            catch (Exception e)
+            {
+                errors.Add("Error: " + e);
+            }
+            finally
+            {
+                conn.Dispose();
+            }
+
+            return courseList;
+        }
+    }
+}
