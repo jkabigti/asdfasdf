3f6863817c3da1fee35e558813738572c18c1026
kjl011 <kjl011@acsmail.ucsd.edu>
2014-08-15 14:55:45 -0700

    enrollment repository

---
+++ asdfasdf-master/SL136/IDAL/IEnrollmentRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace IRepository
+{
+    using System.Collections.Generic;
+
+    using POCO;
+
+    class IEnrollmentRepository
+    {
+        List<Enrollment> GetEnrollments(int scheduleId, ref List<string> errors);
+    }
+}
