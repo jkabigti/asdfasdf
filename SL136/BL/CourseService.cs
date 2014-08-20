a9ce66c877ceba2f34449603ea6945a9863f967f
naytunchen <ralphchen19@gmail.com>
2014-08-20 16:50:24 -0700

    Added BL to Course and Schedule

--- SL136/BL/CourseService.cs
+++ SL136/BL/CourseService.cs
@@ -1,5 +1,7 @@
 ﻿namespace Service
 {
+    using System;
+
     using System.Collections.Generic;
 
     using IRepository;
@@ -18,6 +20,52 @@
         public List<Course> GetCourseList(ref List<string> errors)
         {
             return this.repository.GetCourseList(ref errors);
+        }
+
+        public void AddPrereq(Course course, Course prereq, ref List<string> errors)
+        {
+            if(course == null)
+            {
+                // Throw error message
+                errors.Add("Add Unsuccessful: Invalid course");
+                throw new ArgumentException();
+            }
+            if(prereq == null)
+            {
+                // Throw error message
+                errors.Add("Add Unsuccessful: Invalid Prereq course");
+                throw new ArgumentException();
+            }
+
+            this.repository.AddPrereq(course, prereq, ref errors);
+        }
+
+        public void EditPrereq(Course course, Course prereq, ref List<string> errors)
+        {
+            if(course == null)
+            {
+                // Throw error message
+                errors.Add("Edit Unsuccessful: Invalid course");
+                throw new ArgumentException();
+            }
+            if(prereq == null)
+            {
+                // Throw error message
+                errors.Add("Edit Unsuccessful: Invalid Prereq course");
+                throw new ArgumentException();
+            }
+            this.repository.EditPrereq(course, prereq, ref errors);
+        }
+
+        public void DeletePrereq(Course course, ref List<string> errors)
+        {
+            if( course == null )
+            {
+                // Throw error message
+                errors.Add("Delete Unsuccessful: Invalid course");
+                throw new ArgumentException();
+            }
+            this.repository.DeletePrereq(course, ref errors);
         }
     }
 }
--- SL136/BL/ScheduleService.cs
+++ SL136/BL/ScheduleService.cs
@@ -1,5 +1,7 @@
 ﻿namespace Service
 {
+    using System;
+
     using System.Collections.Generic;
 
     using IRepository;
@@ -18,6 +20,74 @@
         public List<Schedule> GetScheduleList(string year, string quarter, ref List<string> errors)
         {
             return this.repository.GetScheduleList(year, quarter, ref errors);
+        }
+
+        public void AddSchedule(Schedule sch, int sch_day_id, int sch_time_id, int instr_id, ref List<string> errors)
+        {
+            if( sch == null)
+            {
+                // Throw Errror
+                errors.Add("Add Unsuccessful: Invalid Schedule information");
+                throw new ArgumentException();
+            }
+            if(sch_day_id == null)
+            {
+                // Throw Errror
+                errors.Add("Add Unsuccessful: Invalid Schedule Day ID");
+                throw new ArgumentException();
+            }
+            if(sch_time_id == null)
+            {
+                // Throw Errror
+                errors.Add("Add Unsuccessful: Invalid Schedule Time ID");
+                throw new ArgumentException();
+            }
+            if(instr_id == null)
+            {
+                // Throw Errror
+                errors.Add("Add Unsuccessful: Invalid Instructor ID");
+                throw new ArgumentException();
+            }
+            this.repository.AddSchedule(sch, sch_day_id, sch_time_id, instr_id, ref errors);
+        }
+
+        public void DeleteSchedule(Schedule sch, ref List<string> errors)
+        {
+            if( sch == null )
+            {
+                errors.Add("Delete Unsuccessful: Invalid schedule information");
+                throw new ArgumentException();
+            }
+            this.repository.DeleteSchedule(sch, ref errors);
+        }
+
+        public void EditSchedule(Schedule sch, int sch_day_id, int sch_time_id, int instr_id, ref List<string> errors)
+        {
+            if (sch == null)
+            {
+                // Throw Errror
+                errors.Add("Edit Unsuccessful: Invalid Schedule information");
+                throw new ArgumentException();
+            }
+            if (sch_day_id == null)
+            {
+                // Throw Errror
+                errors.Add("Edit Unsuccessful: Invalid Schedule Day ID");
+                throw new ArgumentException();
+            }
+            if (sch_time_id == null)
+            {
+                // Throw Errror
+                errors.Add("Edit Unsuccessful: Invalid Schedule Time ID");
+                throw new ArgumentException();
+            }
+            if (instr_id == null)
+            {
+                // Throw Errror
+                errors.Add("Edit Unsuccessful: Invalid Instructor ID");
+                throw new ArgumentException();
+            }
+            this.repository.EditSchedule(sch, sch_day_id, sch_time_id, instr_id, ref errors);
         }
     }
 }
