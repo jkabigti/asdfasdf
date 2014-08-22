5fb985fe3e4c489e4a169516dc8b823099e45755
jkabigti <jkabigti@ucsd.edu>
2014-08-21 19:33:06 -0700

    fixed merge conflict

--- SL136/BL/AnnouncementService.cs
+++ SL136/BL/AnnouncementService.cs
@@ -3,34 +3,6 @@ namespace Service
     using System;
     using System.Collections.Generic;
     using System.Text.RegularExpressions;
-<<<<<<< HEAD
-    using IRepository;
-    using POCO;
-
-    public class AnnouncementService
-    {
-        private readonly IAnnouncementRepository repository;
-
-        public AnnouncementService(IAnnouncementRepository repository)
-        {
-            this.repository = repository;
-        }
-
-        public void AddAnnouncement(Announcement announcement, ref List<string> errors)
-        {
-            if (announcement == null)
-            {
-                errors.Add("Announcement cannot be null");
-            }
-
-            if (announcement.Text.Length == 0)
-            {
-                errors.Add("Please include text for announcement");
-            }
-
-            Match m = Regex.Match(announcement.Date, @"^((0[1-9]|1[012])[-](19|20)\d\d[-](0?[1-9]|[12][0-9]|3[01])(\x20)(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5[0-9]$");
-            if (!m.Success)
-=======
 	using IRepository;
 	using POCO;
 
@@ -59,7 +31,6 @@ namespace Service
             
             bool b = Regex.IsMatch(announcement.Date, @"^((0[1-9]|1[012])[-](19|20)\d\d[-](0?[1-9]|[12][0-9]|3[01])(\x20)(0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]:[0-5[0-9]$)");
             if (!b)
->>>>>>> origin/master
             {
                 errors.Add("Announcement date must be formatted as yy-MM-dd HH:mm:ss");
                 return;
