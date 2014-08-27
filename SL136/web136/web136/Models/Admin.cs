2fff496f5d7e1b427281909b01b0f00a944a529d
kjl011 <kjl011@acsmail.ucsd.edu>
2014-08-26 18:17:18 -0700

    Admin model

---
+++ SL136/web136/web136/Models/Admin.cs
@@ -0,0 +1,13 @@
+namespace Web136.Models
+{
+    using System.Collections.Generic;
+
+    public class Admin
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
---
+++ SL136/web136/web136/Models/Announcements.cs
@@ -0,0 +1,14 @@
+namespace Web136.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public class Announcement
+    {
+        public int ID { get; set; }
+
+        public string Text { get; set; }
+
+        public string Date { get; set; }
+    }
+}
