55e682594cb043843e08640049b0c77ef6ab139b
kjl011 <kjl011@acsmail.ucsd.edu>
2014-08-26 18:15:05 -0700

    Added Announcements, Enrollment, Request Models

---
+++ SL136/web136/web136/Models/Enrollment.cs
@@ -0,0 +1,15 @@
+namespace Web136.Models
+{
+    using System.Collections.Generic;
+
+    public class Enrollment
+    {
+        public string StudentId { get; set; }
+
+        public int ScheduleId { get; set; }
+
+        public string Grade { get; set; }
+
+        public float GradeValue { get; set; }
+    }
+}
---
+++ SL136/web136/web136/Models/Request.cs
@@ -0,0 +1,13 @@
+namespace Web136.Models
+{
+    using System.Collections.Generic;
+
+    public class Request
+    {
+        public string StudentId { get; set; }
+
+        public int ScheduleId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
