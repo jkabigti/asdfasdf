2375dfed1d7d48089536095600054e76d61cafbd
jkabigti <jkabigti@ucsd.edu>
2014-09-02 17:42:20 -0700

    started knockout for admin

--- web136/web136/Scripts/136Libraries/ViewModels/AdminViewModel.js
+++ web136/web136/Scripts/136Libraries/ViewModels/AdminViewModel.js
@@ -38,6 +38,32 @@ define(['Models/AdminModel'], function (adminModel) {
             });
 
         };
+        
+        this.GetAllSchedule = function () {
+            var adminModelObj = new adminModel();
+            var scheduleListViewModel = ko.observableArray();
+
+            adminModelObj.GetAll(function (scheduleList) {
+                scheduleListViewModel.removeAll();
+
+                for (var i = 0; i < scheduleList.length; i++) {
+                    scheduleListViewModel.push({
+                        year: scheduleList[i].Year,
+                        quarter: scheduleList[i].Quarter,
+                        session: scheduleList[i].Session,
+                        course_title: scheduleList[i].CourseTitle,
+                        course_description: scheduleList[i].CourseDescription,
+                        course_id: scheduleList[i].CourseId,
+                        schedule_id: scheduleList[i].ScheduleId,
+                    });
+                }
+
+                if (initialBind) {
+                    ko.applyBindings({ viewModel: scheduleListViewModel }, document.getElementById("divScheduleListContent"));
+                    initialBind = false; // this is to prevent binding multiple time because "Delete" functio calls GetAll again
+                }
+            });
+        };
 
     }
     return AdminViewModel;
